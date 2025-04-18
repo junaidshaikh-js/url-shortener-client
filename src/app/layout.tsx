55b6c7d1ac40e9e4ea99194bccd7e3d48f09203e
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2025-04-18 16:42:20 +0530

    feat: add theme variables, css reset and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
 import './globals.css'
+
+const inter = Inter({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
   title: 'URL Shortener',
@@ -12,8 +20,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" data-theme="light" className={inter.variable}>
+      <div id="root">
+        <body>{children}</body>
+      </div>
     </html>
   )
 }
